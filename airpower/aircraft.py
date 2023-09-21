4c7d4ea98d156e1170b4aca353779867d5f822de
alanwatsonforster <68709385+alanwatsonforster@users.noreply.github.com>
2023-09-21 08:28:32 -0600

    Implemented additional C and D codes.

--- airpower/aircraft.py
+++ airpower/aircraft.py
@@ -130,6 +130,8 @@ class Aircraft:
 
 
   def _adjustaltitude(self, altitudechange):
+    # Here we do altitude arithmetic, ensuring that _altitude stays as an
+    # integer and keeping track of fractions in _altitudecarry.
     self._altitude = self._altitude + self._altitudecarry + altitudechange
     if altitudechange >= 0:
       self._altitudecarry = self._altitude - math.floor(self._altitude)
@@ -165,8 +167,9 @@ class Aircraft:
 
     self._report("--- start of turn ---")
     self._report("%d FPs available." % self._nfp)
-    self._report("initial azimuth  = %s." % apazimuth.toazimuth(self._facing))
-    self._report("initial altitude = %5.2f (%+.02f) %s" % (self._altitude, self._altitudecarry, apaltitude.altitudeband(self._altitude)))
+    self._report("initial azimuth        = %s" % apazimuth.toazimuth(self._facing))
+    self._report("initial altitude       = %2.0f %s" % (self._altitude, apaltitude.altitudeband(self._altitude)))
+    self._report("initial altitude carry = %+.02f" % (self._altitudecarry))
 
     if s != "":
       self.next(s)
@@ -178,32 +181,52 @@ class Aircraft:
 
     actions = [
 
+      # This table is searched in order, so put longer codes before shorter 
+      # ones that are prefixes (e.g., put C2 before C and D3/4 before D3).
+
       ["H"   , lambda : self._H()],
 
+      ["C⅛"  , lambda : self._C(1/8)],
+      ["C1/8", lambda : self._C(1/8)],
       ["C¼"  , lambda : self._C(1/4)],
       ["C1/4", lambda : self._C(1/4)],
+      ["C⅜"  , lambda : self._C(3/8)],
+      ["C3/8", lambda : self._C(3/8)],
       ["C½"  , lambda : self._C(1/2)],
       ["C1/2", lambda : self._C(1/2)],
+      ["C⅝"  , lambda : self._C(5/8)],
+      ["C5/8", lambda : self._C(5/8)],
       ["C¾"  , lambda : self._C(3/4)],
       ["C3/4", lambda : self._C(3/4)],
+      ["C⅞"  , lambda : self._C(7/8)],
+      ["C7/8", lambda : self._C(7/8)],
       ["C2"  , lambda : self._C(2)],
       ["C"   , lambda : self._C(1)],
 
+      ["D⅛"  , lambda : self._D(1/8)],
+      ["D1/8", lambda : self._D(1/8)],
       ["D¼"  , lambda : self._D(1/4)],
       ["D1/4", lambda : self._D(1/4)],
+      ["D⅜"  , lambda : self._D(3/8)],
+      ["D3/8", lambda : self._D(3/8)],
       ["D½"  , lambda : self._D(1/2)],
       ["D1/2", lambda : self._D(1/2)],
+      ["D⅝"  , lambda : self._D(5/8)],
+      ["D5/8", lambda : self._D(5/8)],
       ["D¾"  , lambda : self._D(3/4)],
       ["D3/4", lambda : self._D(3/4)],
+      ["D⅞"  , lambda : self._D(7/8)],
+      ["D7/8", lambda : self._D(7/8)],
+      ["D3"  , lambda : self._D(3)],
       ["D2"  , lambda : self._D(2)],
       ["D"   , lambda : self._D(1)],
 
-      ["L60" , lambda : self._L(60)],
       ["L90" , lambda : self._L(90)],
+      ["L60" , lambda : self._L(60)],
       ["L"   , lambda : self._L(30)],
 
-      ["R60" , lambda : self._R(60)],
       ["R90" , lambda : self._R(90)],
+      ["R60" , lambda : self._R(60)],
       ["R"   , lambda : self._R(30)],
 
     ]
@@ -234,7 +257,7 @@ class Aircraft:
       lastx = self._x
       lasty = self._y
 
-    self._report("%d HFPs and %d VFPs used." % (self._ihfp, self._ivfp))
+    self._report("%d FPs used (%d HFPs and %d VFPs)." % (self._ifp, self._ihfp, self._ivfp))
       
     if self._ifp < self._nfp:
 
@@ -244,10 +267,11 @@ class Aircraft:
 
     elif self._ifp == self._nfp:
 
-      self._report("all %d FPs used." % (self._nfp))
+      self._report("all FPs used.")
 
-      self._report("final azimuth    = %s." % apazimuth.toazimuth(self._facing))
-      self._report("final altitude   = %5.2f (%+.02f) %s" % (self._altitude, self._altitudecarry, apaltitude.altitudeband(self._altitude)))
+      self._report("final azimuth        = %s" % apazimuth.toazimuth(self._facing))
+      self._report("final altitude       = %2.0f %s" % (self._altitude, apaltitude.altitudeband(self._altitude)))
+      self._report("final altitude carry = %+.02f" % (self._altitudecarry))
       if apaltitude.altitudeband(self._initialaltitude) != apaltitude.altitudeband(self._altitude):
         self._report("altitude band changed from %s to %s." % (apaltitude.altitudeband(self._initialaltitude), apaltitude.altitudeband(self._altitude)))
       self._report("--- end of turn ---")
