d1eaf4cfce75689fff78bdb75efb60023445a2f3
alanwatsonforster <68709385+alanwatsonforster@users.noreply.github.com>
2023-09-22 19:05:20 -0600

    Reworked azimuth.

--- airpower/azimuth.py
+++ airpower/azimuth.py
@@ -1,35 +1,61 @@
 _northfacing = 90
 
-def setnorth(sense):
+def setnorth(orientation):
+
+  """
+  Set the orientation of north. The orientation argument can be one of the 
+  strings "up", "down", "right", or "left".
+  """
+
   global _northfacing
-  if sense == "right":
+  if orientation == "right":
     _northfacing = 0
-  elif sense == "up":
+  elif orientation == "up":
     _northfacing = 90
-  elif sense == "left":
+  elif orientation == "left":
     _northfacing = 180
-  elif sense == "down":
+  elif orientation == "down":
     _northfacing = 270
   else:
-    raise ValueError("\"%s\" is not a valid orientation.")
+    raise ValueError("\"%s\" is not a valid orientation for north.")
 
 def tofacing(azimuth):
+
+  """
+  Return the facing corresponding to the azimuth argument, which can be one of 
+  the strings "N", "NNE", "ENE", "E", "ESE", "SSE", "S", "SSW", "WSW", "W", 
+  "WNW", or "NNW", or a number, specifying the azimuth in degrees from north 
+  through east.
+
+  Note that "NE", "SE", "SW", and "NW" are not valid azimuths.
+  """
+
   named = {
     "N":   0, "NNE":  30, "ENE":  60,
     "E":  90, "ESE": 120, "SSE": 150, 
     "S": 180, "SSW": 210, "WSW": 240, 
     "W": 270, "WNW": 300, "NNW": 330, 
   }
-  superseded = {
+  obsolete = {
     "NE": "ENE", "SE": "ESE", "SW": "WSW", "NW": "WNW"
   }
+
   if azimuth in named:
-    azimuth = named[azimuth]
-  elif azimuth in superseded:
-    raise ValueError("use azimuth \"%s\" instead of \"%s\"" % (superseded[azimuth], azimuth))
-  return (_northfacing - azimuth) % 360
+    return (_northfacing - named[azimuth]) % 360
+  elif azimuth in obsolete:
+    raise ValueError("%s is not a valid azimuth (use %s instead)." % (azimuth, superseded[azimuth]))
+  else:
+    return (_northfacing - azimuth) % 360
 
 def fromfacing(facing):
+
+  """
+  Return the azimuth corresponding to the facing argument. If the azimuth
+  is a multiple of 30 degrees, the corresponding string "N", "NNE", "ENE", "E", 
+  "ESE", "SSE", "S", "SSW", "WSW", "W", "WNW", or "NNW" is returned. Otherwise,
+  a number is returned, giving the azimuth in degrees from north through east.
+  """
+
   azimuth = (_northfacing - facing) % 360
   if azimuth % 30 == 0:
     named = ["N", "NNE", "ENE", "E", "ESE", "SSE", "S", "SSW", "WSW", "W", "WNW", "NNW"]
