b0ee34370e062f204a5535edc7cb299fb9f41f4c
alanwatsonforster <68709385+alanwatsonforster@users.noreply.github.com>
2023-09-23 11:49:52 -0600

    Implement speedbrakes.

--- airpower/aircraft.py
+++ airpower/aircraft.py
@@ -19,12 +19,13 @@ class Aircraft:
 
     self._turn          = 0
     self._name          = name
+
     self._x             = x
     self._y             = y
     self._facing        = facing
     self._altitude      = altitude
     self._altitudecarry = 0
-    self._ifp           = None
+
     self._destroyed     = False
     self._leftmap       = False
 
@@ -40,16 +41,9 @@ class Aircraft:
   def __str__(self):
     return "[name: %s]" % self._name
 
-  def _restore(self, i):
-    self._x, self._y, self._facing, self._altitude, self._altitudecarry, self._destroyed, self._leftmap = self._saved[i]
+   ##############################################################################
 
-  def _save(self, i):
-    if len(self._saved) == i:
-      self._saved.append(None)
-    self._saved[i] = (self._x, self._y, self._facing, self._altitude, self._altitudecarry, self._destroyed, self._leftmap)
-
-  def _maxprevturn(self):
-    return len(self._saved) - 1
+  # Drawing
 
   def _drawflightpath(self, lastx, lasty):
     apdraw.drawflightpath(lastx, lasty, self._x, self._y)
@@ -57,6 +51,10 @@ class Aircraft:
   def _drawaircraft(self, when):
     apdraw.drawaircraft(self._x, self._y, self._facing, self._name, self._altitude, when)
         
+  ##############################################################################
+
+  # Reporting
+
   def _formatposition(self):
     if apmap.isonmap(self._x, self._y):
       sheet = apmap.tosheet(self._x, self._y)
@@ -72,7 +70,7 @@ class Aircraft:
     if self._turn == 0:
       return ""
     else:
-      return "FP %d of %d" % (self._ifp, self._nfp)
+      return "FP %d of %.1f" % (self._ifp, self._nfp)
 
   def _report(self, s):
     print("%s: turn %-2d : %s" % (self._name, self._turn, s))
@@ -81,13 +79,13 @@ class Aircraft:
     print()
 
   def _reportposition(self):
-    self._report("%-11s : %-16s : %s" % ("", "", self._formatposition()))
+    self._report("%-13s : %-16s : %s" % ("", "", self._formatposition()))
 
   def _reportactionsandposition(self, action):
-    self._report("%-11s : %-16s : %s" % (self._formatifp(), action, self._formatposition()))
+    self._report("%-13s : %-16s : %s" % (self._formatifp(), action, self._formatposition()))
   
   def _reportevent(self, s):
-    self._report("%-11s : %s" % (self._formatifp(), s))
+    self._report("%-13s : %s" % (self._formatifp(), s))
 
   def _reportstartofturn(self):
     self._report("--- start of turn %s -- " % self._turn)
@@ -99,12 +97,13 @@ class Aircraft:
   def _reportstatus(self, when):
 
     if when != "start":
-       self._reportevent("%d HFPs and %d VFPs used." % (self._ihfp, self._ivfp))
+       self._reportevent("%d HFPs, %d VFPs, and %.1f SFPs used." % (self._ihfp, self._ivfp, self._sfp))
 
     if when == "start":
       self._reportactionsandposition("")
 
     if when != "start":
+      self._reportevent("FP carry is %.1f." % (self._nfp - self._ifp))
       self._reportevent("altitude carry is %s." % apaltitude.formataltitudecarry(self._altitudecarry))
 
     altitudeband = apaltitude.altitudeband(self._altitude)
@@ -113,6 +112,10 @@ class Aircraft:
     elif when == "end":
       if altitudeband!= self._initialaltitudeband:
         self._report("altitude band changed from %s to %s." % (self._initialaltitudeband, altitudeband))
+
+  #############################################################################
+
+  # Elements
 
   def _H(self):
     dx = {
@@ -202,12 +205,23 @@ class Aircraft:
   def _C(self, altitudechange):
     self._altitude, self._altitudecarry = apaltitude.adjustaltitude(self._altitude, self._altitudecarry, +altitudechange)
 
+  def _S(self, sfp):
+    if self._sfp != 0:
+      raise ValueError("speedbrakes can only be used once per turn.")
+    fpremaining = self._nfp - self._ifp
+    if sfp > fpremaining:
+      raise ValueError("attempt to use speedbrakes to eliminate %s FPs but only %s are remaining." % (sfp, fpremaining))
+    self._sfp = sfp
+    self._nfp -= sfp
+
   def _K(self):
     self._reportevent("aircraft has been killed.")
     self._destroyed = True
 
   def _A(self, what):
     self._reportevent("attack with %s." % what)
+
+  ##############################################################################
 
   def checkforterraincollision(self):
     altitudeofterrain = apaltitude.terrainaltitude()
@@ -222,6 +236,21 @@ class Aircraft:
       self._reportevent("aircraft has left the map.")
       self._leftmap = True
   
+ ##############################################################################
+
+  # Turn management
+  
+  def _restore(self, i):
+    self._x, self._y, self._facing, self._altitude, self._altitudecarry, self._destroyed, self._leftmap = self._saved[i]
+
+  def _save(self, i):
+    if len(self._saved) == i:
+      self._saved.append(None)
+    self._saved[i] = (self._x, self._y, self._facing, self._altitude, self._altitudecarry, self._destroyed, self._leftmap)
+
+  def _maxprevturn(self):
+    return len(self._saved) - 1
+
   def start(self, turn, nfp, actions):
 
     if turn > self._maxprevturn() + 1:
@@ -232,6 +261,7 @@ class Aircraft:
     self._ifp = 0
     self._ihfp = 0
     self._ivfp = 0
+    self._sfp = 0
     self._restore(turn - 1)
 
     self._reportstartofturn()
@@ -265,68 +295,74 @@ class Aircraft:
 
       ["H"   , lambda : self._H()   , lambda : None],
 
-      ["C⅛"  , lambda : self._C(1/8), lambda : None],
-      ["C1/8", lambda : self._C(1/8), lambda : None],
-      ["C¼"  , lambda : self._C(1/4), lambda : None],
-      ["C1/4", lambda : self._C(1/4), lambda : None],
-      ["C⅜"  , lambda : self._C(3/8), lambda : None],
-      ["C3/8", lambda : self._C(3/8), lambda : None],
-      ["C½"  , lambda : self._C(1/2), lambda : None],
-      ["C1/2", lambda : self._C(1/2), lambda : None],
-      ["C⅝"  , lambda : self._C(5/8), lambda : None],
-      ["C5/8", lambda : self._C(5/8), lambda : None],
-      ["C¾"  , lambda : self._C(3/4), lambda : None],
-      ["C3/4", lambda : self._C(3/4), lambda : None],
-      ["C⅞"  , lambda : self._C(7/8), lambda : None],
-      ["C7/8", lambda : self._C(7/8), lambda : None],
-      ["CC"  , lambda : self._C(2)  , lambda : None],
-      ["C2"  , lambda : self._C(2)  , lambda : None],
-      ["C"   , lambda : self._C(1)  , lambda : None],
-      ["C1"  , lambda : self._C(1)  , lambda : None],
+      ["C1/8", lambda : self._C(1/8), lambda: None],
+      ["C1/4", lambda : self._C(1/4), lambda: None],
+      ["C3/8", lambda : self._C(3/8), lambda: None],
+      ["C1/2", lambda : self._C(1/2), lambda: None],
+      ["C5/8", lambda : self._C(5/8), lambda: None],
+      ["C3/4", lambda : self._C(3/4), lambda: None],
+      ["C7/8", lambda : self._C(7/8), lambda: None],
+      ["C⅛"  , lambda : self._C(1/8), lambda: None],
+      ["C¼"  , lambda : self._C(1/4), lambda: None],
+      ["C⅜"  , lambda : self._C(3/8), lambda: None],
+      ["C½"  , lambda : self._C(1/2), lambda: None],
+      ["C⅝"  , lambda : self._C(5/8), lambda: None],
+      ["C¾"  , lambda : self._C(3/4), lambda: None],
+      ["C⅞"  , lambda : self._C(7/8), lambda: None],
+      ["C1"  , lambda : self._C(1)  , lambda: None],
+      ["C2"  , lambda : self._C(2)  , lambda: None],
+      ["CC"  , lambda : self._C(2)  , lambda: None],
+      ["C"   , lambda : self._C(1)  , lambda: None],
 
-      ["D⅛"  , lambda : self._D(1/8), lambda : None],
-      ["D1/8", lambda : self._D(1/8), lambda : None],
-      ["D¼"  , lambda : self._D(1/4), lambda : None],
-      ["D1/4", lambda : self._D(1/4), lambda : None],
-      ["D⅜"  , lambda : self._D(3/8), lambda : None],
-      ["D3/8", lambda : self._D(3/8), lambda : None],
-      ["D½"  , lambda : self._D(1/2), lambda : None],
-      ["D1/2", lambda : self._D(1/2), lambda : None],
-      ["D⅝"  , lambda : self._D(5/8), lambda : None],
-      ["D5/8", lambda : self._D(5/8), lambda : None],
-      ["D¾"  , lambda : self._D(3/4), lambda : None],
-      ["D3/4", lambda : self._D(3/4), lambda : None],
-      ["D⅞"  , lambda : self._D(7/8), lambda : None],
-      ["D7/8", lambda : self._D(7/8), lambda : None],
-      ["DDD" , lambda : self._D(3)  , lambda : None],
-      ["D3"  , lambda : self._D(3)  , lambda : None],
-      ["DD"  , lambda : self._D(2)  , lambda : None],
-      ["D2"  , lambda : self._D(2)  , lambda : None],
-      ["D"   , lambda : self._D(1)  , lambda : None],
-      ["D1"  , lambda : self._D(1)  , lambda : None],
+      ["D1/8", lambda : self._D(1/8), lambda: None],
+      ["D1/4", lambda : self._D(1/4), lambda: None],
+      ["D3/8", lambda : self._D(3/8), lambda: None],
+      ["D1/2", lambda : self._D(1/2), lambda: None],
+      ["D5/8", lambda : self._D(5/8), lambda: None],
+      ["D3/4", lambda : self._D(3/4), lambda: None],
+      ["D7/8", lambda : self._D(7/8), lambda: None],
+      ["D⅛"  , lambda : self._D(1/8), lambda: None],
+      ["D¼"  , lambda : self._D(1/4), lambda: None],
+      ["D⅜"  , lambda : self._D(3/8), lambda: None],
+      ["D½"  , lambda : self._D(1/2), lambda: None],
+      ["D⅝"  , lambda : self._D(5/8), lambda: None],
+      ["D¾"  , lambda : self._D(3/4), lambda: None],
+      ["D⅞"  , lambda : self._D(7/8), lambda: None],
+      ["D1"  , lambda : self._D(1)  , lambda: None],
+      ["D2"  , lambda : self._D(2)  , lambda: None],
+      ["D3"  , lambda : self._D(3)  , lambda: None],
+      ["DDD" , lambda : self._D(3)  , lambda: None],
+      ["DD"  , lambda : self._D(2)  , lambda: None],
+      ["D"   , lambda : self._D(1)  , lambda: None],
 
-      ["LLL" , lambda : self._L(90) , lambda : None],
-      ["L90" , lambda : self._L(90) , lambda : None],
-      ["LL"  , lambda : self._L(60) , lambda : None],
-      ["L60" , lambda : self._L(60) , lambda : None],
-      ["L"   , lambda : self._L(30) , lambda : None],
-      ["L30" , lambda : self._L(30) , lambda : None],
+      ["L90" , lambda : self._L(90) , lambda: None],
+      ["L60" , lambda : self._L(60) , lambda: None],
+      ["L30" , lambda : self._L(30) , lambda: None],
+      ["LLL" , lambda : self._L(90) , lambda: None],
+      ["LL"  , lambda : self._L(60) , lambda: None],
+      ["L"   , lambda : self._L(30) , lambda: None],
 
-      ["RRR" , lambda : self._R(90) , lambda : None],
-      ["R90" , lambda : self._R(90) , lambda : None],
-      ["RR"  , lambda : self._R(60) , lambda : None],
-      ["R60" , lambda : self._R(60) , lambda : None],
-      ["R"   , lambda : self._R(30) , lambda : None],
-      ["R30" , lambda : self._R(30) , lambda : None],
+      ["R90" , lambda : self._R(90) , lambda: None],
+      ["R60" , lambda : self._R(60) , lambda: None],
+      ["R30" , lambda : self._R(30) , lambda: None],
+      ["RRR" , lambda : self._R(90) , lambda: None],
+      ["RR"  , lambda : self._R(60) , lambda: None],
+      ["R"   , lambda : self._R(30) , lambda: None],
 
-      ["K"   , lambda : None        , lambda : self._K()],
+      ["S1/2", lambda: self._S(1/2) , lambda: None],
+      ["S½"  , lambda: self._S(1/2) , lambda: None],
+      ["S1"  , lambda: self._S(1)   , lambda: None],
+      ["SS"  , lambda: self._S(1)   , lambda: None],
+      ["S"   , lambda: self._S(1/2) , lambda: None],
 
-      ["AGN" , lambda : None        , lambda : self._A("guns")],
-      ["AGP" , lambda : None        , lambda : self._A("gun pod")],
-      ["ARK" , lambda : None        , lambda : self._A("rockets")],
-      ["ARP" , lambda : None        , lambda : self._A("rocket pods")],
+      ["K"   , lambda : None        , lambda: self._K()],
 
-      ["/"   , lambda : None        , lambda : None]
+      ["AGN" , lambda : None        , lambda: self._A("guns")],
+      ["AGP" , lambda : None        , lambda: self._A("gun pod")],
+      ["ARK" , lambda : None        , lambda: self._A("rockets")],
+      ["ARP" , lambda : None        , lambda: self._A("rocket pods")],
+
+      ["/"   , lambda : None        , lambda: None]
 
     ]
 
@@ -388,7 +424,7 @@ class Aircraft:
     assert aphex.isvalidfacing(self._x, self._y, self._facing)
     assert apaltitude.isvalidaltitude(self._altitude)
 
-    if self._ifp == self._nfp or self._destroyed or self._leftmap:
+    if self._ifp + 1 > self._nfp or self._destroyed or self._leftmap:
 
       self._reportstatus("end")
       self._drawaircraft("end")
