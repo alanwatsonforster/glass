6f3c168d1125af211ff10a5dddeec009a4e833b7
alanwatsonforster <68709385+alanwatsonforster@users.noreply.github.com>
2023-10-01 20:38:30 -0600

    Remove the explicit turn argument.

--- airpower/turn.py
+++ airpower/turn.py
@@ -2,21 +2,14 @@ import airpower.aircraft as apaircraft
 import airpower.log as aplog
 
 _turn = None
-_maxturn = 0
 
 def restart():
   global _turn
-  _turn = 1
+  _turn = 0
 
-def startturn(turn):
+def startturn():
   global _turn
-  global _maxturn
-  if not isinstance(turn, int) or turn < 1:
-    raise RuntimeError("invalid turn %d." % turn)
-  if turn > _maxturn + 1:
-    raise RuntimeError("attempt to start turn %d out of sequence." % turn)
-  _turn = turn
-  _maxturn = max(_maxturn, _turn)
+  _turn += 1
   aplog.log("--- start of turn %d ---" % _turn)
   aplog.logbreak()
   apaircraft._allstartturn()
@@ -26,13 +19,6 @@ def endturn():
   global _turn
   aplog.log("--- end of turn %d ---" % _turn)
   aplog.logbreak()
-  _turn = None
 
 def turn():
   return _turn
-
-def restart():
-  global _turn
-  global _maxturn
-  _turn = None
-  _maxturn = 0
