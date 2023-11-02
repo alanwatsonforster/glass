d2efcd0a30566b565a44f132c90a9d60db80169d
alanwatsonforster <68709385+alanwatsonforster@users.noreply.github.com>
2023-11-02 15:51:12 -0600

    Add an option not to draw terrain.

--- apengine/_tests/infrastructure.py
+++ apengine/_tests/infrastructure.py
@@ -19,9 +19,9 @@ def asserterror(error):
     print("actual   error: %r" % aplog._error)
     assert aplog._error == error
 
-def starttestsetup(sheets=[["A1"],["A2"]], north="up", variants=[], verbose=False):
+def starttestsetup(sheets=[["A1"],["A2"]], north="up", variants=[], drawterrain=False, verbose=False):
   aplog._silent = not verbose
-  ap.startsetup(None, sheets=sheets, north=north, variants=variants)
+  ap.startsetup(None, sheets=sheets, north=north, drawterrain=drawterrain, variants=variants)
 
 def endtestsetup():
   ap.endsetup()
