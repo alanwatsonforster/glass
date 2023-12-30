45a94df61eab0f86ff67e8c7a63b99596d08cb03
alanwatsonforster <68709385+alanwatsonforster@users.noreply.github.com>
2023-12-30 13:36:43 -0600

    Check we can read all map sheet files.

---
+++ apxo/tests/a5.py
@@ -0,0 +1,14 @@
+from apxo.tests.infrastructure import *
+startfile(__file__, "maps")
+
+# Check we can at least read all of the maps.
+
+for sheet in "ABCDEFGHIJKLMNOPQRSTUVWXYZ":
+  starttestsetup(sheets=[[sheet]])
+
+for letter in "ABCD":
+  for number in range(1, 7):
+    sheet = "%s%d" % (letter, number)
+    starttestsetup(sheets=[[sheet]])
+
+endfile(__file__)
