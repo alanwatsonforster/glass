e623026202c4abf58b5a695b4876719f05248116
alanwatsonforster <68709385+alanwatsonforster@users.noreply.github.com>
2024-08-25 20:16:41 -0700

    Formatting.

--- apxo/speed.py
+++ apxo/speed.py
@@ -539,7 +539,7 @@ def _startaircraftspeed(
 
         if A.speed() >= m1speed(A.altitudeband()):
             maxspeedbrakes += 2.0
-            
+
         if speedbrakes is True:
             speedbrakes = maxspeedbrakes
 
