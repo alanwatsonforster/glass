83e3d89e8e8961605faabe5ee451c1bad5dd8f76
alanwatsonforster <68709385+alanwatsonforster@users.noreply.github.com>
2024-12-20 09:36:11 -0600

    Split out ground unit attacks.

--- apxo/groundunit/__init__.py
+++ apxo/groundunit/__init__.py
@@ -1,7 +1,6 @@
 import apxo.draw as apdraw
 import apxo.element as apelement
 import apxo.gameturn as apgameturn
-import apxo.geometry as apgeometry
 import apxo.log as aplog
 import apxo.marker as apmarker
 
@@ -97,34 +96,6 @@ class groundunit(apelement.element):
 
     def isgroundunit(self):
         return True
-
-    #############################################################################
-
-    def _attackaircraft(self, target):
-
-        if self.isusingbarragefire():
-
-            if apgeometry.horizontalrange(self, target) > 1:
-                raise RuntimeError("target is out of range of barrage fire.")
-            if target.altitude() > self._barragefiremaximumaltitude():
-                raise RuntimeError("target is above barrage fire.")
-            self.logwhenwhat(
-                "", "%s attacks %s with barrage fire." % (self.name(), target.name())
-            )
-
-        else:
-
-            self.logwhenwhat(
-                "", "%s attacks %s with aimed fire." % (self.name(), target.name())
-            )
-            self.logwhenwhat(
-                "", "range to target is %d." % apgeometry.range(self, target)
-            )
-            self.logwhenwhat(
-                "", "altitude to target is %d." % (target.altitude() - self.altitude())
-            )
-
-        # TODO: plotted fire
 
     #############################################################################
 
@@ -234,3 +205,7 @@ class groundunit(apelement.element):
     def _barragefiremaximumaltitude(self):
         "Return the maximum altitude of barrage fire."
         return self.altitude() + self._barragefirealtitude
+
+    ############################################################################
+
+    from apxo.groundunit.attack import _attackaircraft
---
+++ apxo/groundunit/attack.py
@@ -0,0 +1,31 @@
+import apxo.geometry as apgeometry
+
+#############################################################################
+
+def _attackaircraft(self, target):
+
+    if self.isusingbarragefire():
+
+        if apgeometry.horizontalrange(self, target) > 1:
+            raise RuntimeError("target is out of range of barrage fire.")
+        if target.altitude() > self._barragefiremaximumaltitude():
+            raise RuntimeError("target is above barrage fire.")
+        self.logwhenwhat(
+            "", "%s attacks %s with barrage fire." % (self.name(), target.name())
+        )
+
+    else:
+
+        self.logwhenwhat(
+            "", "%s attacks %s with aimed fire." % (self.name(), target.name())
+        )
+        self.logwhenwhat(
+            "", "range to target is %d." % apgeometry.range(self, target)
+        )
+        self.logwhenwhat(
+            "", "altitude to target is %d." % (target.altitude() - self.altitude())
+        )
+
+    # TODO: plotted fire
+
+#############################################################################
