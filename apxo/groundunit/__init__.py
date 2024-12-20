8647d12732ebd46ad9fb54658c2e57338a583028
alanwatsonforster <68709385+alanwatsonforster@users.noreply.github.com>
2024-12-20 09:45:43 -0600

    Split out ground unit damage.

--- apxo/groundunit/__init__.py
+++ apxo/groundunit/__init__.py
@@ -99,46 +99,6 @@ class groundunit(apelement.element):
 
     #############################################################################
 
-    def _damage(self):
-        if self._damagelevel == 0:
-            if self.issuppressed():
-                return "S"
-            else:
-                return ""
-        elif self._damagelevel == 1:
-            if self.issuppressed():
-                return "D+S"
-            else:
-                return "D"
-        elif self._damagelevel == 2:
-            if self.issuppressed():
-                return "2D+S"
-            else:
-                return "2D"
-        else:
-            return "K"
-
-    def _takedamage(self, damage):
-        self._suppressionlevel = 2
-        if damage == "S":
-            pass
-        elif damage == "D":
-            self._damagelevel += 1
-        elif damage == "2D":
-            self._damagelevel += 2
-        elif damage == "K":
-            self._damagelevel += 3
-        else:
-            raise RuntimeError("invalid damage %r" % damage)
-
-    def _takedamageconsequences(self):
-        if self.isusingbarragefire():
-            self.logwhenwhat("", "%s ceases barrage fire." % self.name())
-            self._barragefiremarker._remove()
-            self._barragefiremarker = None
-
-    #############################################################################
-
     def _properties(self):
         return []
 
@@ -209,3 +169,5 @@ class groundunit(apelement.element):
     ############################################################################
 
     from apxo.groundunit.attack import _attackaircraft
+    
+    from apxo.groundunit.damage import _damage, _takedamage, _takedamageconsequences
