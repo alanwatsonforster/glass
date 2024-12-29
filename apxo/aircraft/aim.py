e0f339346f8e04dc5aefe2a8c415c669f0ab6a0c
alanwatsonforster <68709385+alanwatsonforster@users.noreply.github.com>
2024-12-29 11:44:30 -0600

    Report aiming requirements.

--- apxo/aircraft/aim.py
+++ apxo/aircraft/aim.py
@@ -2,6 +2,7 @@
 
 import apxo.hexcode as aphexcode
 import apxo.log as aplog
+from apxo.rounding import onethirdfromtable, twothirdsfromtable, roundup
 
 ################################################################################
 
@@ -22,6 +23,21 @@ def aim(self, target, *args, **kwargs):
             raise RuntimeError("invalid target for aiming.")
         else:
             self.logwhenwhat("", "starts aiming at %s." % target.name())
+        if self.bombsystem() == "manual":
+            requirement = max(1, roundup(twothirdsfromtable(self.speed())))
+        elif self.bombsystem() == "ballistic":
+            requirement = max(1, roundup(self.speed() / 2))
+        else:
+            requirement = max(1, roundup(onethirdfromtable(self.speed())))
+        self.logcomment("aiming requirement is %d %s." % (
+            requirement,
+            aplog.plural(requirement, "FP", "FPs")
+        ))
+        modifierrequirement =  max(1, roundup(onethirdfromtable(self.speed())))
+        self.logcomment("aiming modifier requirement is %d %s." % (
+            modifierrequirement,
+            aplog.plural(modifierrequirement, "FP", "FPs")
+        ))
         self._aimingtarget = target
     except RuntimeError as e:
         aplog.logexception(e)
