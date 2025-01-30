ef87f3400b9e4e2db6b300d2af848c797a8aca83
Alan Watson <68709385+alanwatsonforster@users.noreply.github.com>
2025-01-30 00:29:30 -0600

    Fix edge case for notes.

--- apxo/log.py
+++ apxo/log.py
@@ -110,27 +110,30 @@ def lognote(note, who=None, writefile=True):
         # Convert tabs to spaces (following the normal Python rules)
         # and split into a list of lines:
         lines = s.expandtabs().splitlines()
-        # Determine minimum indentation (first line doesn't count):
-        indent = None
-        for line in lines[1:]:
-            stripped = line.lstrip()
-            if stripped:
-                if indent is None:
-                    indent = len(line) - len(stripped)
-                else:
-                    indent = min(indent, len(line) - len(stripped))
-        # Remove indentation (first line is special):
-        trimmed = [lines[0].strip()]
-        if indent is not None:
+        if len(lines) == 0:
+            return [""]
+        else:
+            # Determine minimum indentation (first line doesn't count):
+            indent = None
             for line in lines[1:]:
-                trimmed.append(line[indent:].rstrip())
-        # Strip off trailing and leading blank lines:
-        while trimmed and not trimmed[-1]:
-            trimmed.pop()
-        while trimmed and not trimmed[0]:
-            trimmed.pop(0)
-        # Return the lines.
-        return trimmed
+                stripped = line.lstrip()
+                if stripped:
+                    if indent is None:
+                        indent = len(line) - len(stripped)
+                    else:
+                        indent = min(indent, len(line) - len(stripped))
+            # Remove indentation (first line is special):
+            trimmed = [lines[0].strip()]
+            if indent is not None:
+                for line in lines[1:]:
+                    trimmed.append(line[indent:].rstrip())
+            # Strip off trailing and leading blank lines:
+            while trimmed and not trimmed[-1]:
+                trimmed.pop()
+            while trimmed and not trimmed[0]:
+                trimmed.pop(0)
+            # Return the lines.
+            return trimmed
 
     if note is not None:
         for line in splitandtrim(note):
