ed2f968af4f91768142bb3b5f0f2ca9c47254275
Alan Watson Forster <68709385+alanwatsonforster@users.noreply.github.com>
2025-03-29 20:25:56 -0600

    Makefile intergration.

--- aircraftdatacards/pull-descriptions.py
+++ aircraftdatacards/pull-descriptions.py
@@ -2,20 +2,31 @@ import glob
 import os.path
 import json
 
-print("formatting aircraft data files.")
+print("pulling descriptions from aircraft data files.")
 paths = os.path.join(os.path.dirname(__file__), "..", "apxo", "aircraftdata", "*.json")
+
+descriptionpath = os.path.join(os.path.dirname(__file__), "description.json")
+
+description = {}
 
 for path in sorted(glob.glob(paths)):
     basename = os.path.basename(path)
     if basename[0] == "_":
         continue
-    print("formatting \"%s\"" % basename)
+    print("pulling description from \"%s\"" % basename)
     try:
         with open(path, "r") as f:
             text = f.read()
         object = json.loads(text)
+        if "description" in object:
+            description[basename] = object["description"]
+        else:
+            description[basename] = ""
         newtext = json.dumps(object, indent=4, sort_keys=False, ensure_ascii=False)
         with open(path, "w") as f:
             f.write(newtext)
     except json.JSONDecodeError as e:
         print("error in file \"%s\": %s" % (path, e))
+
+with open(descriptionpath, "w") as f:
+    f.write(json.dumps(description, indent=4, sort_keys=True, ensure_ascii=False))
--- aircraftdatacards/push-descriptions.py
+++ aircraftdatacards/push-descriptions.py
@@ -2,18 +2,26 @@ import glob
 import os.path
 import json
 
-print("formatting aircraft data files.")
+print("pushing descriptions to aircraft data files.")
 paths = os.path.join(os.path.dirname(__file__), "..", "apxo", "aircraftdata", "*.json")
+
+descriptionpath = os.path.join(os.path.dirname(__file__), "description.json")
+
+with open(descriptionpath, "r") as f:
+    description = json.loads(f.read())
 
 for path in sorted(glob.glob(paths)):
     basename = os.path.basename(path)
     if basename[0] == "_":
         continue
-    print("formatting \"%s\"" % basename)
+    print("pushing description to \"%s\"" % basename)
     try:
         with open(path, "r") as f:
             text = f.read()
         object = json.loads(text)
+        if not basename in description:
+            raise RuntimeError("no description for %s" % basename)
+        object["description"] = description[basename]
         newtext = json.dumps(object, indent=4, sort_keys=False, ensure_ascii=False)
         with open(path, "w") as f:
             f.write(newtext)
