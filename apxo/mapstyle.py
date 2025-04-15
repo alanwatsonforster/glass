e1ff7a1ca3c16a4988b73456ddd39e795b4b6afd
Alan Watson Forster <68709385+alanwatsonforster@users.noreply.github.com>
2025-04-14 19:22:12 -0600

    Rewrite mapstyle.py in functional style.

--- apxo/mapstyle.py
+++ apxo/mapstyle.py
@@ -1,7 +1,10 @@
 ################################################################################
 
-_style = {}
+__all__ = [ "getstyle" ]
 
+################################################################################
+
+_style = {}
 
 def getstyle(stylename):
     """
@@ -27,7 +30,7 @@ def getstyle(stylename):
 # The colors in the base style are bright red to make it clear when they are not
 # overridden.
 
-_basestyle = {
+basestyle = {
     "base": "land",
     "leveloffset": 0,
     "wilderness": False,
@@ -60,7 +63,6 @@ _basestyle = {
 
 ################################################################################
 
-
 def lighten(color, f):
     """
     Return a lightened color.
@@ -68,8 +70,8 @@ def lighten(color, f):
     :param color: The color parameter is the original color. It must be a tuple
         or list of three floats.
 
-    :param f: The f parameter is the mixing factor with white. 
-    
+    :param f: The f parameter is the mixing factor with white.
+
     :return: The lightened color that is (1-f) times white mixed with f times
         the original color.
     """
@@ -83,11 +85,11 @@ def darken(color, f):
     :param color: The color parameter is the original color. It must be a tuple
         or list of three floats.
 
-    :param f: The f parameter is the mixing factor with black. 
-    
+    :param f: The f parameter is the mixing factor with black.
+
     :return: The darkened color that is (1-f) times black mixed with f times the
         original color.
-    """    
+    """
     return list(min(1, f * x) for x in color)
 
 
@@ -108,74 +110,64 @@ def equivalentgray(color):
 ################################################################################
 
 
-def _setlandcolors(style, basecolor, f):
-
-    level0color = lighten(basecolor, dilution[0])
-    level1color = lighten(basecolor, dilution[1])
-    level2color = lighten(basecolor, dilution[2])
-    level3color = lighten(basecolor, dilution[3])
+def _withlandcolors(style, basecolor, f):
+    level0color = lighten(basecolor, f[0])
+    level1color = lighten(basecolor, f[1])
+    level2color = lighten(basecolor, f[2])
+    level3color = lighten(basecolor, f[3])
     level0ridgecolor = level1color
     level1ridgecolor = level2color
     level2ridgecolor = level3color
-    style.update(
-        {
-            "level0color": level0color,
-            "level1color": level1color,
-            "level2color": level2color,
-            "level3color": level3color,
-            "level0ridgecolor": level0ridgecolor,
-            "level1ridgecolor": level1ridgecolor,
-            "level2ridgecolor": level2ridgecolor,
-        }
-    )
+    return style | {
+        "level0color": level0color,
+        "level1color": level1color,
+        "level2color": level2color,
+        "level3color": level3color,
+        "level0ridgecolor": level0ridgecolor,
+        "level1ridgecolor": level1ridgecolor,
+        "level2ridgecolor": level2ridgecolor,
+    }
 
 
-_setlandcolors(0, 1, 2)
-def _setwatercolors(style):
+def _withwatercolors(style):
     watercolor = [0.77, 0.89, 0.95]
-    # Darken the water to 100% of the grey value of level 0. Do not lighten it.
-    level0color = style["level0color"]
+    # Darken the water to the grey value of level 0. Do not lighten it.
     watergrayvalue = equivalentgray(watercolor)[0]
-    targetgrayvalue = 1.00 * equivalentgray(level0color)[0]
-    if watergrayvalue > targetgrayvalue:
-        watercolor = darken(watercolor, targetgrayvalue / watergrayvalue)
+    level0color = style["level0color"]
+    level0grayvalue = 1.00 * equivalentgray(level0color)[0]
+    if watergrayvalue > level0grayvalue:
+        watercolor = darken(watercolor, level0grayvalue / watergrayvalue)
     wateroutlinecolor = darken(watercolor, 0.80)
-    style.update(
-        {
-            "watercolor": watercolor,
-            "wateroutlinecolor": wateroutlinecolor,
-        }
-    )
+    return style | {
+        "watercolor": watercolor,
+        "wateroutlinecolor": wateroutlinecolor,
+    }
 
 
-def _setforestcolors(style, factor, alpha=0.5):
+def _withforestcolors(style, factor, alpha=0.5):
     forestcolor = darken([0.50, 0.65, 0.50], factor)
     forestalpha = alpha
-    style.update(
-        {
-            "forestcolor": forestcolor,
-            "forestalpha": forestalpha,
-        }
-    )
+    return style | {
+        "forestcolor": forestcolor,
+        "forestalpha": forestalpha,
+    }
 
 
-def _setgraybuiltcolors(style):
+def _withgraybuiltcolors(style):
     level0color = style["level0color"]
     gray = equivalentgray(level0color)
     darkergray = darken(gray, 0.7)
-    style.update(
-        {
-            "urbancolor": gray,
-            "urbanoutlinecolor": darkergray,
-            "roadcolor": gray,
-            "roadoutlinecolor": darkergray,
-            "dockcolor": gray,
-            "dockoutlinecolor": darkergray,
-        }
-    )
+    return style | {
+        "urbancolor": gray,
+        "urbanoutlinecolor": darkergray,
+        "roadcolor": gray,
+        "roadoutlinecolor": darkergray,
+        "dockcolor": gray,
+        "dockoutlinecolor": darkergray,
+    }
 
 
-def _setgrayhexcolors(style, allforest=False):
+def _withgrayhexcolors(style, allforest=False):
     level0color = style["level0color"]
     gray = equivalentgray(level0color)
     labelcolor = darken(gray, 0.7)
@@ -183,225 +175,240 @@ def _setgrayhexcolors(style, allforest=False):
         hexcolor = darken(gray, 0.7 * 0.7)
     else:
         hexcolor = darken(gray, 0.7)
-    style.update(
-        {
-            "hexcolor": hexcolor,
-            "hexalpha": 1.0,
-            "labelcolor": labelcolor,
-        }
-    )
+    return style | {
+        "hexcolor": hexcolor,
+        "hexalpha": 1.0,
+        "labelcolor": labelcolor,
+    }
 
 
-def _setwhitemegahexcolors(style, alpha):
+def _withwhitemegahexcolors(style, alpha):
     megahexcolor = [1.00, 1.00, 1.00]
     megahexalpha = alpha
-    style.update(
-        {
-            "megahexcolor": megahexcolor,
-            "megahexalpha": megahexalpha,
-        }
-    )
+    return style | {
+        "megahexcolor": megahexcolor,
+        "megahexalpha": megahexalpha,
+    }
 
 
-def _setgraymegahexcolors(style, alpha):
+def _withgraymegahexcolors(style, alpha):
     megahexcolor = [0.00, 0.00, 0.00]
     megahexalpha = alpha
-    style.update(
-        {
-            "megahexcolor": megahexcolor,
-            "megahexalpha": megahexalpha,
-        }
-    )
+    return style | {
+        "megahexcolor": megahexcolor,
+        "megahexalpha": megahexalpha,
+    }
 
 
-def _setforest(style, value=True):
-    style.update({"forest": value})
+def _withforest(style, value=True):
+    style = style | {
+        "forest": value,
+    }
     if value == "all":
-        _setgrayhexcolors(style, allforest=True)
+        style = _withgrayhexcolors(style, allforest=True)
+    return style
 
 
-def _setwilderness(style, value=True):
-    style.update({"wilderness": value})
+def _withwilderness(style, value=True):
+    return style | {
+        "wilderness": value,
+    }
 
 
-def _setwater(style, value=True):
-    style.update({"water": value})
+def _withwater(style, value=True):
+    return style | {
+        "water": value,
+    }
 
 
-def _setmaxurbansize(style, value):
-    style.update({"maxurbansize": value})
+def _withmaxurbansize(style, value):
+    return style | {
+        "maxurbansize": value,
+    }
 
 
-def _setsnowycolors(style):
-    _setlandcolors(style, [0.85, 0.85, 0.85], [1 / 20, 1 / 2, 2 / 2, 3 / 2])
+def _withsnowycolors(style):
+    style = _withlandcolors(style, [0.85, 0.85, 0.85], [1 / 20, 1 / 2, 2 / 2, 3 / 2])
     watercolor = (0.83, 0.89, 0.95)
-    style.update(
-        {
-            "watercolor": watercolor,
-        }
-    )
-    if style["forest"] == "all":
-        _setforestcolors(style, 1.0, 0.7)
-        _setgraymegahexcolors(style, 0.04)
-    else:
-        _setgraymegahexcolors(style, 0.02)
-
-
-def _setfrozencolors(style):
-    level0color = style["level0color"]
-    style.update(
-        {
-            "watercolor": level0color,
-            "wateroutlinecolor": level0color,
-        }
-    )
-
-
-def _setleveloffset(style, leveloffset):
-    style.update(
-        {
-            "leveloffset": leveloffset,
-        }
-    )
-
-
-def _setislands(style):
-    style.update(
-        {
-            "water": "islands",
-        }
-    )
-
-
-################################################################################
-
-_thisstyle = _style["airstrike"] = _basestyle.copy()
-
-# The Air Strike colors don't fit into the scheme of increasingly darker
-# shades of the same color, so are hard-wired.
-_thisstyle.update(
-    {
-        "level0color": [0.75, 0.85, 0.725],
-        "level1color": [0.82, 0.75, 0.65],
-        "level2color": [0.77, 0.65, 0.55],
-        "level3color": [0.62, 0.52, 0.44],
-    }
-)
-_thisstyle.update(
-    {
-        "level0ridgecolor": lighten([0.50, 0.70, 0.45], 4 / 6),
-        "level1ridgecolor": _thisstyle["level2color"],
-        "level2ridgecolor": _thisstyle["level3color"],
-    }
-)
-
-_setforestcolors(_thisstyle, 0.8)
-_setwatercolors(_thisstyle)
-_setgraybuiltcolors(_thisstyle)
-_setgrayhexcolors(_thisstyle)
-_setwhitemegahexcolors(_thisstyle, 0.10)
-
-################################################################################
-
-_thisstyle = _style["airsuperiority"] = _style["airstrike"].copy()
-_setwater(_thisstyle, "all")
-watercolor = _thisstyle["watercolor"]
-_thisstyle.update(
-    {
+    style = style | {
         "watercolor": watercolor,
-        "hexcolor": darken(watercolor, 0.7),
-        "hexalpha": 1.0,
-        "labelcolor": darken(watercolor, 0.7),
     }
+    if style["forest"] == "all":
+        style = _withforestcolors(style, 1.0, 0.7)
+        style = _withgraymegahexcolors(style, 0.04)
+    else:
+        style = _withgraymegahexcolors(style, 0.02)
+    return style
+
+
+def _withfrozencolors(style):
+    level0color = style["level0color"]
+    return style | {
+        "watercolor": level0color,
+        "wateroutlinecolor": level0color,
+    }
+
+
+def _withleveloffset(style, leveloffset):
+    return style | {
+        "leveloffset": leveloffset,
+    }
+
+
+def _withislands(style):
+    return style | {
+        "water": "islands",
+    }
+
+
+################################################################################
+
+style = basestyle
+
+# The Air Strike land colors don't fit into the scheme of increasingly darker
+# shades of the same color, so are hard-wired.
+
+style = style | {
+    "level0color": [0.75, 0.85, 0.725],
+    "level1color": [0.82, 0.75, 0.65],
+    "level2color": [0.77, 0.65, 0.55],
+    "level3color": [0.62, 0.52, 0.44],
+}
+style = style | {
+    "level0ridgecolor": lighten([0.50, 0.70, 0.45], 4 / 6),
+    "level1ridgecolor": style["level2color"],
+    "level2ridgecolor": style["level3color"],
+}
+
+style = _withforestcolors(style, 0.8)
+style = _withwatercolors(style)
+style = _withgraybuiltcolors(style)
+style = _withgrayhexcolors(style)
+style = _withwhitemegahexcolors(style, 0.10)
+
+_style["airstrike"] = style
+
+################################################################################
+
+
+style = _style["airstrike"]
+style = _withwater(style, "all")
+watercolor = style["watercolor"]
+style = style | {
+    "watercolor": watercolor,
+    "hexcolor": darken(watercolor, 0.7),
+    "hexalpha": 1.0,
+    "labelcolor": darken(watercolor, 0.7),
+}
+style = _withwhitemegahexcolors(style, 0.10)
+
+_style["airsuperiority"] = style
+
+################################################################################
+
+style = basestyle
+
+style = _withlandcolors(
+    style, [0.50, 0.70, 0.45], [3 / 6, 4 / 6, 5 / 6, 6 / 6]
 )
-_setwhitemegahexcolors(_thisstyle, 0.10)
+style = _withforestcolors(style, 0.8)
+style = _withwatercolors(style)
+style = _withgraybuiltcolors(style)
+style = _withgrayhexcolors(style)
+style = _withwhitemegahexcolors(style, 0.10)
+
+_style["temperate"] = style
 
 ################################################################################
 
-_thisstyle = _style["temperate"] = _basestyle.copy()
+style = _style["temperate"]
+style = _withforest(style, "all")
+style = _withmaxurbansize(style, 4)
 
-_setlandcolors(_thisstyle, [0.50, 0.70, 0.45], [3 / 6, 4 / 6, 5 / 6, 6 / 6])
-_setforestcolors(_thisstyle, 0.8)
-_setwatercolors(_thisstyle)
-_setgraybuiltcolors(_thisstyle)
-_setgrayhexcolors(_thisstyle)
-_setwhitemegahexcolors(_thisstyle, 0.10)
+_style["temperateforest"] = style
 
 ################################################################################
 
-_thisstyle = _style["temperateforest"] = _style["temperate"].copy()
-_setforest(_thisstyle, "all")
-_setmaxurbansize(_thisstyle, 4)
+style = _style["temperate"]
+style = _withforest(style, False)
+style = _withwilderness(style)
+
+_style["tundra"] = style
 
 ################################################################################
 
-_thisstyle = _style["tundra"] = _style["temperate"].copy()
-_setforest(_thisstyle, False)
-_setwilderness(_thisstyle)
+style = _style["temperate"]
+style = _withforest(style, "all")
+style = _withwilderness(style)
+
+_style["borealforest"] = style
 
 ################################################################################
 
-_thisstyle = _style["borealforest"] = _style["temperate"].copy()
-_setforest(_thisstyle, "all")
-_setwilderness(_thisstyle)
+style = _style["temperate"]
+style = _withwater(style, "all")
+
+_style["water"] = style
 
 ################################################################################
 
-_thisstyle = _style["water"] = _style["temperate"].copy()
-_setwater(_thisstyle, "all")
+style = _style["temperate"]
+style = _withlandcolors(
+    style, [0.50, 0.70, 0.45], [4 / 6, 5 / 6, 6 / 6, 7 / 6]
+)
+style = _withforestcolors(style, 0.6)
+style = _withwatercolors(style)
+style = _withgraybuiltcolors(style)
+style = _withgrayhexcolors(style)
+style = _withwhitemegahexcolors(style, 0.08)
+
+_style["tropical"] = style
 
 ################################################################################
 
-_thisstyle = _style["tropical"] = _style["temperate"].copy()
-_setlandcolors(_thisstyle, [0.50, 0.70, 0.45], [4 / 6, 5 / 6, 6 / 6, 7 / 6])
-_setforestcolors(_thisstyle, 0.6)
-_setwatercolors(_thisstyle)
-_setgraybuiltcolors(_thisstyle)
-_setgrayhexcolors(_thisstyle)
-_setwhitemegahexcolors(_thisstyle, 0.08)
+style = _style["tropical"]
+style = _withforest(style, "all")
+style = _withmaxurbansize(style, 4)
+
+_style["tropicalforest"] = style
 
 ################################################################################
 
-_thisstyle = _style["tropicalforest"] = _style["tropical"].copy()
-_setforest(_thisstyle, "all")
-_setmaxurbansize(_thisstyle, 4)
+style = _style["temperate"]
+style = _withlandcolors(
+    style, [0.80, 0.76, 0.64], [1 / 3, 2 / 3, 3 / 3, 4 / 3]
+)
+style = _withforestcolors(style, 1.0)
+style = _withwatercolors(style)
+style = _withgraybuiltcolors(style)
+style = _withgrayhexcolors(style)
+style = _withwhitemegahexcolors(style, 0.22)
+style = _withwater(style, "arid")
+
+_style["arid"] = style
 
 ################################################################################
 
-_thisstyle = _style["arid"] = _style["temperate"].copy()
-_setlandcolors(_thisstyle, [0.80, 0.76, 0.64], [1 / 3, 2 / 3, 3 / 3, 4 / 3])
-_setforestcolors(_thisstyle, 1.0)
-_setwatercolors(_thisstyle)
-_setgraybuiltcolors(_thisstyle)
-_setgrayhexcolors(_thisstyle)
-_setwhitemegahexcolors(_thisstyle, 0.22)
-_setwater(_thisstyle, "arid")
+style = _style["arid"]
+style = _withwilderness(style)
+style = _withwater(style, "desert")
+style = _withforest(style, False)
 
-################################################################################
-
-_thisstyle = _style["desert"] = _style["arid"].copy()
-_setwilderness(_thisstyle)
-_setwater(_thisstyle, "desert")
-_setforest(_thisstyle, False)
+_style["desert"] = style
 
 ################################################################################
 
 for style in list(_style.keys()):
     if style != "airsuperiority" and style != "airstrike":
-        _thisstyle = _style["snowy" + style] = _style[style].copy()
-        _setsnowycolors(_thisstyle)
-        _thisstyle = _style["frozen" + style] = _style["snowy" + style].copy()
-        _setfrozencolors(_thisstyle)
+        _style["snowy" + style] = _withsnowycolors(_style[style])
+        _style["frozen" + style] = _withfrozencolors(_style["snowy" + style])
 
 ################################################################################
 
 for style in list(_style.keys()):
     if style != "airsuperiority" and style != "airstrike":
-        _thisstyle = _style[style + "hills"] = _style[style].copy()
-        _setleveloffset(_thisstyle, -1)
-        _thisstyle = _style[style + "plain"] = _style[style].copy()
-        _setleveloffset(_thisstyle, -3)
-        _thisstyle = _style[style + "islands"] = _style[style].copy()
-        _setislands(_thisstyle)
+        _style[style + "hills"] = _withleveloffset(_style[style], -1)
+        _style[style + "plain"] = _withleveloffset(_style[style], -3)
+        _style[style + "islands"] = _withislands(_style[style])
 
 ################################################################################
