e4a99f394ff92eb04285a82eec2fdb4483820e60
Alan Watson Forster <68709385+alanwatsonforster@users.noreply.github.com>
2025-06-26 21:18:00 -0600

    Working on #236: Add Air Superiority stores

--- src/glass/aircraft/stores.py
+++ src/glass/aircraft/stores.py
@@ -98,50 +98,50 @@ _storedict = {
     "BB/Cratering/Cluster/1000" : [ "BB", 1000, 3.5 ],
 
     # Soviet
-    "BB/FAB-50": ["BB", 110, 1.0],
-    "BB/FAB-100": ["BB", 225, 1.5],
-    "BB/FAB-250": ["BB", 550, 2.0],
-    "BB/FAB-500": ["BB", 1100, 3.0],
-    "BB/FAB-750": ["BB", 1650, 3.5],
-    "BB/FAB-1000": ["BB", 2200, 4.0],
-    "BB/ZAB-100": ["BB", 225, 1.5],
-    "BB/ZAB-250": ["BB", 550, 2.0],
-    "BB/ZAB-1000": ["BB", 2200, 4.0],
-    "BB/HD/PLAB-250": ["BB", 550, 2.0],
+    "BB/FAB-50": [ "BB", 110, 1.0 ],
+    "BB/FAB-100": [ "BB", 225, 1.5 ],
+    "BB/FAB-250": [ "BB", 550, 2.0 ],
+    "BB/FAB-500": [ "BB", 1100, 3.0 ],
+    "BB/FAB-750": [ "BB", 1650, 3.5 ],
+    "BB/FAB-1000": [ "BB", 2200, 4.0 ],
+    "BB/ZAB-100": [ "BB", 225, 1.5 ],
+    "BB/ZAB-250": [ "BB", 550, 2.0 ],
+    "BB/ZAB-1000": [ "BB", 2200, 4.0 ],
+    "BB/HD/PLAB-250": [ "BB", 550, 2.0 ],
         
     # US
-    "BB/M30": ["BB", 100, 0.5],
-    "BB/M57": ["BB", 250, 1.5],
-    "BB/M64": ["BB", 500, 2.0],
-    "BB/M65": ["BB", 1000, 3.0],
-    "BB/M66": ["BB", 2000, 4.0],
-    "BB/M74": ["BB", 100, 0.5],
-    "BB/M76": ["BB", 500, 1.5],
-    "BB/HD/BLU-1": ["BB", 750, 2.5],  # HD only from errata.
-    "BB/M117": ["BB", 750, 2.0],
-    "BB/HD/M117": ["BB", 750, 2.0],
-    "BB/M118": ["BB", 3000, 5.0],
-    "BB/Mk81": ["BB", 250, 1.0],
-    "BB/HD/Mk81": ["BB", 250, 1.0],
-    "BB/Mk82": ["BB", 500, 1.5],
-    "BB/HD/Mk82": ["BB", 500, 1.5],
-    "BB/Mk83": ["BB", 1000, 2.5],
-    "BB/HD/Mk83": ["BB", 1000, 2.5],
-    "BB/Mk84": ["BB", 2000, 3.0],
-    "BB/HD/BLU-10": ["BB", 250, 1.0],
-    "BB/HD/BLU-11": ["BB", 500, 1.5],
-    "BB/HD/BLU-27": ["BB", 750, 2.5],  # Same properties as BLU-1.
-    "BB/HD/Mk77": ["BB", 750, 2.0],
-    "BB/HD/Mk79": ["BB", 1000, 2.5],
-    "BB/CBU-20": ["BB", 500, 1.5],
-    "BB/HD/CBU-20": ["BB", 500, 1.5],
-    "BB/CBU-41": ["BB", 850, 2.0],
-    "BB/HD/CBU-41": ["BB", 850, 2.0],
-    "BB/CBU-58": ["BB", 800, 2.0],
-    "BB/CBU-59": ["BB", 750, 2.0],
-    "BB/HD/CBU-59": ["BB", 750, 2.0],
-    "BB/CBU-71": ["BB", 800, 2.0],
-    "BB/HD/CBU-71": ["BB", 800, 2.0],
+    "BB/M30": [ "BB", 100, 0.5 ],
+    "BB/M57": [ "BB", 250, 1.5 ],
+    "BB/M64": [ "BB", 500, 2.0 ],
+    "BB/M65": [ "BB", 1000, 3.0 ],
+    "BB/M66": [ "BB", 2000, 4.0 ],
+    "BB/M74": [ "BB", 100, 0.5 ],
+    "BB/M76": [ "BB", 500, 1.5 ],
+    "BB/HD/BLU-1": [ "BB", 750, 2.5 ],  # HD only from errata.
+    "BB/M117": [ "BB", 750, 2.0 ],
+    "BB/HD/M117": [ "BB", 750, 2.0 ],
+    "BB/M118": [ "BB", 3000, 5.0 ],
+    "BB/Mk81": [ "BB", 250, 1.0 ],
+    "BB/HD/Mk81": [ "BB", 250, 1.0 ],
+    "BB/Mk82": [ "BB", 500, 1.5 ],
+    "BB/HD/Mk82": [ "BB", 500, 1.5 ],
+    "BB/Mk83": [ "BB", 1000, 2.5 ],
+    "BB/HD/Mk83": [ "BB", 1000, 2.5 ],
+    "BB/Mk84": [ "BB", 2000, 3.0 ],
+    "BB/HD/BLU-10": [ "BB", 250, 1.0 ],
+    "BB/HD/BLU-11": [ "BB", 500, 1.5 ],
+    "BB/HD/BLU-27": [ "BB", 750, 2.5 ],  # Same properties as BLU-1.
+    "BB/HD/Mk77": [ "BB", 750, 2.0 ],
+    "BB/HD/Mk79": [ "BB", 1000, 2.5 ],
+    "BB/CBU-20": [ "BB", 500, 1.5 ],
+    "BB/HD/CBU-20": [ "BB", 500, 1.5 ],
+    "BB/CBU-41": [ "BB", 850, 2.0 ],
+    "BB/HD/CBU-41": [ "BB", 850, 2.0 ],
+    "BB/CBU-58": [ "BB", 800, 2.0 ],
+    "BB/CBU-59": [ "BB", 750, 2.0 ],
+    "BB/HD/CBU-59": [ "BB", 750, 2.0 ],
+    "BB/CBU-71": [ "BB", 800, 2.0 ],
+    "BB/HD/CBU-71": [ "BB", 800, 2.0 ],
 
     ################################################################################
 
@@ -211,14 +211,14 @@ _storedict = {
     "RK/250"               : [ "RK",  250, 1.0 ],
 
     # Soviet
-    "RK/S-8": ["RK", 25, 0.5],
-    "RK/TRS-190": ["RK", 100, 1.0],
-    "RK/ARS-212": ["RK", 260, 1.0],
-    "RK/S-24": ["RK", 350, 1.5],
+    "RK/S-8": [ "RK", 25, 0.5 ],
+    "RK/TRS-190": [ "RK", 100, 1.0 ],
+    "RK/ARS-212": [ "RK", 260, 1.0 ],
+    "RK/S-24": [ "RK", 350, 1.5 ],
 
     # US
-    "RK/HVAR": ["RK", 140, 1.0],
-    "RK/Tiny Tim": ["RK", 1200, 2.0],
+    "RK/HVAR": [ "RK", 140, 1.0 ],
+    "RK/Tiny Tim": [ "RK", 1200, 2.0 ],
 
     ################################################################################
 
@@ -275,17 +275,17 @@ _storedict = {
     "RP/Large/Medium"      : [ "RP",  500, 3.5 ],
 
     # Soviet
-    "RP/ORO-8K": ["RP", 175, 2.0],
-    "RP/UV-8-57": ["RP", 175, 2.0],
-    "RP/UV-16-57": ["RP", 300, 3.0],
-    "RP/UV-32-57": ["RP", 500, 3.5],
+    "RP/ORO-8K": [ "RP", 175, 2.0 ],
+    "RP/UV-8-57": [ "RP", 175, 2.0 ],
+    "RP/UV-16-57": [ "RP", 300, 3.0 ],
+    "RP/UV-32-57": [ "RP", 500, 3.5 ],
 
     # US
-    "RP/LAU-68": ["RP", 250, 2.0],
-    "RP/LAU-3A": ["RP", 450, 3.0],
-    "RP/LAU-10": ["RP", 550, 3.0],
-    "RP/LAU-33": ["RP", 200, 2.9],  # From errata.
-    "RP/LAU-37": ["RP", 850, 3.5],
+    "RP/LAU-68": [ "RP", 250, 2.0 ],
+    "RP/LAU-3A": [ "RP", 450, 3.0 ],
+    "RP/LAU-10": [ "RP", 550, 3.0 ],
+    "RP/LAU-33": [ "RP", 200, 2.9 ],  # From errata.
+    "RP/LAU-37": [ "RP", 850, 3.5 ],
 
     ################################################################################
 
@@ -310,49 +310,73 @@ _storedict = {
 
     # WRs
 
-    "WR/DR"    : [ "WR",  100, 1.0],
-    "WR/TR"    : [ "WR",  100, 1.0],
-    "WR/MR"    : [ "WR",  200, 2.0],
-    "WR/MDR"   : [ "WR",  100, 1.0],
-    "WR/ARM-DR": [ "WR",  200, 2.0],
+    "WR/DR"    : [ "WR",  100, 1.0 ],
+    "WR/TR"    : [ "WR",  100, 1.0 ],
+    "WR/MR"    : [ "WR",  200, 2.0 ],
+    "WR/MDR"   : [ "WR",  100, 1.0 ],
+    "WR/ARM-DR": [ "WR",  200, 2.0 ],
         
     ################################################################################
 
     # Air-to-air missiles
 
-    # US
-    "IRM/AIM-9B": ["IRM", 160, 1.0],
-    "RHM/AIM-9C": ["RHM", 190, 1.0],
-    "IRM/AIM-9D": ["IRM", 200, 1.0],
-    "IRM/AIM-9E": ["IRM", 170, 1.0],
-    "IRM/AIM-9E2": ["IRM", 170, 1.0],
-    "IRM/AIM-9G": ["IRM", 190, 1.0],
-    "IRM/AIM-9H": ["IRM", 185, 1.0],
-    "IRM/AIM-9J": ["IRM", 175, 1.0],
-    "IRM/AIM-9J3": ["IRM", 175, 1.0],
-    "IRM/AIM-9N": ["IRM", 175, 1.0],
-    "IRM/AIM-9P": ["IRM", 170, 1.0],
-    "IRM/AIM-9P2": ["IRM", 170, 1.0],
-    "IRM/AIM-9P3": ["IRM", 170, 1.0],
-    "IRM/AIM-9P4": ["IRM", 170, 1.0],
-    "IRM/AIM-9L": ["IRM", 190, 1.0],
-    "IRM/AIM-9M": ["IRM", 190, 1.0],
-    "IRM/AIM-9M4": ["IRM", 190, 1.0],
-    "IRM/AIM-9M5": ["IRM", 190, 1.0],
-    "IRM/AIM-9S": ["IRM", 190, 1.0],
-    "IRM/AIM-9X": ["IRM", 190, 1.0],
-    "IRM/AIM-9X-II": ["IRM", 190, 1.0],
-    "IRM/FGW.2": ["IRM", 170, 1.0],
+    # France
+    "IRM/R.550 Magic I" : [ "IRM", 200, 1.0 ],
+    "IRM/R.550 Magic II": [ "IRM", 200, 1.0 ],
+    "RHM/R.530 A"       : [ "RHM", 450, 1.5 ],
+    "IRM/R.530 B"       : [ "IRM", 450, 1.5 ],
+    "RHM/R.530 D"       : [ "RHM", 550, 1.5 ],
+
+    # Germany
+    "IRM/FGW.2": [ "IRM", 170, 1.0 ],
+
+    # Italy
+    "RHM/Aspide"        : [ "RHM", 500, 1.0 ],
 
     # Soviet
-    "IRM/AA-2": ["IRM", 160, 1.0],
-    "IRM/AA-2A": ["IRM", 160, 1.0],
-    "IRM/AA-2B": ["IRM", 180, 1.0],
-    "RHM/AA-2C": ["RHM", 200, 1.0],
-    "IRM/AA-2D": ["IRM", 200, 1.0],
-    "IRM/AA-8": ["IRM", 140, 1.0],
-    "IRM/AA-8B": ["IRM", 140, 1.0],
-    "IRM/AA-8C": ["IRM", 150, 1.0],
+    "IRM/AA-2"          : [ "IRM", 160, 1.0 ],
+    "IRM/AA-2A"         : [ "IRM", 160, 1.0 ],
+    "IRM/AA-2B"         : [ "IRM", 180, 1.0 ],
+    "RHM/AA-2C"         : [ "RHM", 200, 1.0 ],
+    "IRM/AA-2D"         : [ "IRM", 200, 1.0 ],
+    "IRM/AA-3A"         : [ "IRM", 600, 1.5 ],
+    "RHM/AA-3B"         : [ "RHM", 600, 1.5 ],
+    "RHM/AA-7A"         : [ "RHM", 700, 1.5 ],
+    "IRM/AA-7B"         : [ "IRM", 700, 1.5 ],
+    "IRM/AA-8"          : [ "IRM", 140, 1.0 ],
+    "IRM/AA-8B"         : [ "IRM", 140, 1.0 ],
+    "IRM/AA-8C"         : [ "IRM", 150, 1.0 ],
+    "RHM/AA-10A"        : [ "RHM", 550, 1.0 ],
+    "IRM/AA-10B"        : [ "IRM", 550, 1.0 ],
+
+    # UK
+    "IRM/Redtop"        : [ "IRM", 300, 1.0 ],
+    "RHM/Skyflash"      : [ "RHM", 400, 1.0 ],
+        
+    # US
+    "IRM/AIM-9B"        : [ "IRM", 160, 1.0 ],
+    "RHM/AIM-9C"        : [ "RHM", 190, 1.0 ],
+    "IRM/AIM-9D"        : [ "IRM", 200, 1.0 ],
+    "IRM/AIM-9E"        : [ "IRM", 170, 1.0 ],
+    "IRM/AIM-9E2"       : [ "IRM", 170, 1.0 ],
+    "IRM/AIM-9G"        : [ "IRM", 190, 1.0 ],
+    "IRM/AIM-9H"        : [ "IRM", 185, 1.0 ],
+    "IRM/AIM-9J"        : [ "IRM", 175, 1.0 ],
+    "IRM/AIM-9J3"       : [ "IRM", 175, 1.0 ],
+    "IRM/AIM-9N"        : [ "IRM", 175, 1.0 ],
+    "IRM/AIM-9P"        : [ "IRM", 170, 1.0 ],
+    "IRM/AIM-9P2"       : [ "IRM", 170, 1.0 ],
+    "IRM/AIM-9P3"       : [ "IRM", 170, 1.0 ],
+    "IRM/AIM-9P4"       : [ "IRM", 170, 1.0 ],
+    "IRM/AIM-9L"        : [ "IRM", 190, 1.0 ],
+    "IRM/AIM-9M"        : [ "IRM", 190, 1.0 ],
+    "IRM/AIM-9M4"       : [ "IRM", 190, 1.0 ],
+    "IRM/AIM-9M5"       : [ "IRM", 190, 1.0 ],
+    "IRM/AIM-9S"        : [ "IRM", 190, 1.0 ],
+    "IRM/AIM-9X"        : [ "IRM", 190, 1.0 ],
+    "IRM/AIM-9X-II"     : [ "IRM", 190, 1.0 ],
+    "AHM/AIM-120A"      : [ "AHM", 350, 1.0 ],
+    "AHM/AIM-54C"       : [ "AHM", 1000, 2.0 ],
 
 }
 
@@ -535,7 +559,7 @@ def _airtoairlaunch(stores, launched, printer=print):
     if loadstation not in stores:
         raise RuntimeError("load station %s is not loaded." % loadstation)
 
-    if _class(stores[loadstation]) not in ["IRM", "BRM", "RHM", "AHM"]:
+    if _class(stores[loadstation]) not in [ "IRM", "BRM", "RHM", "AHM"]:
         raise RuntimeError(
             "load station %s is not loaded with an air-to-air missile." % loadstation
         )
